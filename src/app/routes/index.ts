5d16bc507458c375e8d8d3c973c3ad85a8280f2c
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-20 14:29:28 +0600

    add to mobile route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { AuthRouter } from "../modules/auth/auth.route";
+import { MobileRoutes } from "../modules/mobile/mobile.route";
 
 const router = express.Router();
 
 // all module route is here
-const AllModuleRoutes = [{ path: "/auth", router: AuthRouter }];
+const AllModuleRoutes = [
+  { path: "/auth", router: AuthRouter },
+  { path: "/mobile", router: MobileRoutes },
+];
 
 AllModuleRoutes.forEach((route) => router.use(route.path, route.router));
 
